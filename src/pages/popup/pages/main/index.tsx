5c8bafb83ebd80f634d81c313c571c60537d52e2
e-roy <ericroupe@gmail.com>
2023-03-18 04:54:30 -0400

    add copy to clipboard

--- src/pages/popup/pages/main/index.tsx
+++ src/pages/popup/pages/main/index.tsx
@@ -1,16 +1,22 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
+import { nip19 } from "nostr-tools";
+import { IoMdCopy } from "react-icons/io";
 
 import { generatePrivateKey, getPublicKey } from "nostr-tools";
 
 export const MainPage = () => {
-  const [privateKey, setPrivateKey] = useState<string | null>(null);
-  const [publicKey, setPublicKey] = useState<string | null>(null);
+  const [privateKey, setPrivateKey] = useState<string>("");
+  const [publicKey, setPublicKey] = useState<string>("");
 
-  const handleGetPrivateKey = async () => {
+  const handleGetKeys = useCallback(() => {
     const pk = generatePrivateKey(); // generate private key
     setPrivateKey(pk);
     const pubKey = getPublicKey(pk); // get public key
-    setPublicKey(pubKey);
+    setPublicKey(nip19.npubEncode(pubKey));
+  }, []);
+
+  const handleCopy = async (key: string) => {
+    await navigator.clipboard.writeText(key);
   };
 
   return (
@@ -21,17 +27,35 @@ export const MainPage = () => {
         </h1>
       </header>
       <div className={`h-96`}>
-        <p className={`text-sm font-medium`}>private key:</p>
-        <p className={`text-sm text-gray-500 break-all`}>{privateKey}</p>
-        <p className={`text-sm font-medium`}>public key:</p>
-        <p className={`text-sm text-gray-500 break-all`}>{publicKey}</p>
+        <div className={`h-32`}>
+          <div className={`text-sm font-medium`}>private key:</div>
+          <div className={`text-sm text-gray-500 break-all flex`}>
+            {privateKey}
+            {privateKey && (
+              <button onClick={() => handleCopy(privateKey)}>
+                <IoMdCopy className={`h-5 w-5 hover:text-gray-700`} />
+              </button>
+            )}
+          </div>
+        </div>
+        <div className={`h-32`}>
+          <div className={`text-sm font-medium`}>public key:</div>
+          <div className={`text-sm text-gray-500 break-all flex`}>
+            {publicKey}
+            {publicKey && (
+              <button onClick={() => handleCopy(publicKey)}>
+                <IoMdCopy className={`h-5 w-5 hover:text-gray-700`} />
+              </button>
+            )}
+          </div>
+        </div>
       </div>
       <div className={`flex justify-center`}>
         <button
           className={`rounded-lg p-2 uppercase bg-primary-500 hover:bg-primary-600 text-white font-medium`}
-          onClick={() => handleGetPrivateKey()}
+          onClick={handleGetKeys}
         >
-          get keys
+          generate a new key pair
         </button>
       </div>
     </div>
